1a85162f62af8ee22c97ce38abe0594c4566b002
Nat VMware <natsumi1.902@gmail.com>
2023-07-16 14:04:24 +0100

    adde new apis.

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -29,19 +29,6 @@ export const getMovie = (args) => {
  });
 };
   
-  // export const getGenres = async () => {
-  //   return fetch(
-  //     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-  //   ).then( (response) => {
-  //     if (!response.ok) {
-  //       throw new Error(response.json().message);
-  //     }
-  //     return response.json();
-  //   })
-  //   .catch((error) => {
-  //     throw error
-  //  });
-  // };
 
   export const getGenres = async () => {
     return fetch(
@@ -101,6 +88,118 @@ export const getMovie = (args) => {
         // console.log(json.results);
         return json.results;
       });
+    };
 
- 
+  export const getPopularMovie = () => {
+    return fetch(
+       `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch popular movies.");
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        throw error;
+      });
   };
+
+  export const getTopRatedMovies = () => {
+    return fetch(
+       `https://api.themoviedb.org/3/movie/top_rated?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch top rated movies.");
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  export const getTrendingMovies = () => {
+    return fetch(
+       `https://api.themoviedb.org/3/movie/trending?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch trending movies.");
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+
+  export const getSimilarMovies = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+
+
+  export const getPopularActors = () => {
+    return fetch(
+       `https://api.themoviedb.org/3/person/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch top rated movies.");
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+  
+  export const getActorImages = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/person/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+
+  export const getActorDetails = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/person/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    ).then( (response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+  
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
+  
